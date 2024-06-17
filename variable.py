2c14e4ea0aebf0c40803abc80dd0eea2af41f58a
kalmux1 <kal.mux.cyber@gmail.com>
2024-06-17 14:29:54 +0530

    Variable Task

--- variable.py
+++ variable.py
@@ -1,4 +1,5 @@
-# VARIABLES TASK IN Beginner Level
+  # VARIABLES TASK IN Beginner Level
+#-------------------------------------#
 
 # 1. Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
 
@@ -7,6 +8,7 @@ print(type (pi))
 
 # It's returninng float as the datatype 
 
+#---------------------------------------------------------------------------------------------------------------------------------#
 
 # 2. Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see.
 
@@ -14,6 +16,7 @@ for = 4   # Kindly Comment out this line for executing rest of the code !
     
 # It will throw an error as the "for" is a reserved keyword used for creating loops. As such, it cannot be used as a variable name.
 
+#-----------------------------------------------------------------------------------------------------------------------------------#
 
 # 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100
 
@@ -25,3 +28,5 @@ si = ((p_amt * roi * time) / 100)  # Simple Intrest ((Principal Amount * Rate Of
 print (si)
 
 # The calculated Simple Intrest is 630.0 
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
