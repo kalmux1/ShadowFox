538e6657d927ca500676bf44faea1bde9dd3f46b
kalmux1 <kal.mux.cyber@gmail.com>
2024-06-17 21:34:51 +0530

    Variable Task

--- Task 1/variable.py
+++ Task 1/variable.py
@@ -1,5 +1,5 @@
-  # VARIABLES TASK IN Beginner Level
-#-------------------------------------#
+                                       # VARIABLES TASK IN Beginner Level
+#-----------------------------------------------------------------------------------------------------------------------------------#
 
 # 1. Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
 
@@ -8,7 +8,7 @@ print(type (pi))
 
 # It's returninng float as the datatype 
 
-#---------------------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------------------#
 
 # 2. Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see.
 
@@ -16,9 +16,10 @@ for = 4   # Kindly Comment out this line for executing rest of the code !
     
 # It will throw an error as the "for" is a reserved keyword used for creating loops. As such, it cannot be used as a variable name.
 
-#-----------------------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------------------------------------#
 
-# 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100
+# 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years.
+#  Formula: Simple Interest = P x R x T / 100
 
 p_amt = 3000   # Principal Amount 
 roi = 7        # Rate Of Intrest
@@ -29,4 +30,4 @@ print (si)
 
 # The calculated Simple Intrest is 630.0 
 
-#---------------------------------------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------------------------#
